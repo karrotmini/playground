0e5de47c5a56840a9dd709786a54090cb5cfe5b8
Hyeseong Kim <tim@daangn.com>
2022-07-22 03:40:59 +0900

    refactor playground-bundle-storage routes

--- playground-bundle-storage/src/lib.rs
+++ playground-bundle-storage/src/lib.rs
@@ -9,63 +9,75 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     utils::set_panic_hook();
 
     Router::new()
-        .get_async("/bundle/:id/manifest", |_req, ctx| async move {
-            if let Some(id) = ctx.param("id") {
-                let storage = ctx.kv("KV_BUNDLE_STORAGE").unwrap();
-                let key = "bundle:".to_string() + id;
-                let content = storage.get(key.as_str()).bytes().await.unwrap();
-                let response = match content {
-                    None => {
-                        Response::error("Bundle not found", 404)
-                    }
-                    Some(bytes) => {
-                        match Package::from_bytes(bytes) {
-                            Ok(Package { manifest: Some(manifest), .. }) => {
-                                Response::from_json(&manifest)
-                            },
-                            Ok(_) => {
-                                Response::from_json(&json!(null))
-                            },
-                            Err(err) => {
-                                Response::error(err.to_string(), 500)
-                            },
-                        }
-                    }
-                };
-                return response;
-            }
-            Response::error("Bad Request", 400)
-        })
-        .post_async("/bundle/:id/upload", |mut req, ctx| async move {
-            if let Some(id) = ctx.param("id") {
-                let storage = ctx.kv("KV_BUNDLE_STORAGE")?;
-                let form = req.form_data().await?;
-                if let Some(FormEntry::File(file)) = form.get("content") {
-                    let key = "bundle:".to_string() + id;
-                    let bytes = file.bytes().await?;
-                    return match Package::from_bytes(bytes.clone()) {
-                        Ok(_) => {
-                            storage.put_bytes(key.as_str(), bytes.as_slice())?.execute().await?;
-                            Response::ok("")
-                        },
-                        Err(err) => {
-                           Response::error(err.to_string(), 400)
-                        }
-                    }
-                }
-            }
-            Response::error("Bad Request", 400)
-        })
-        .post_async("/bundle/:id/connect_hostname", |mut req, ctx| async move {
-            if let Some(id) = ctx.param("id") {
-                let storage = ctx.kv("KV_BUNDLE_STORAGE")?;
-                let form = req.form_data().await?;
-                if let Some(FormEntry::Field(hostname)) = form.get("hostname") {
-                    let key = "hostname:".to_string() + hostname.as_str();
-                    storage.put(key.as_str(), id.as_str())?;
-                }
-            }
-            Response::error("Bad Request", 400)
-        })
+        .get_async("/bundle/:id/manifest", get_bundle_manifest)
+        .post_async("/bundle/:id/upload", upload_bundle_content)
+        .post_async("/bundle/:id/connect_hostname", connect_bundle_hostname)
         .run(req, env).await
 }
+
+async fn get_bundle_manifest(mut _req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let storage = ctx.kv("KV_BUNDLE_STORAGE")?;
+    if let Some(id) = ctx.param("id") {
+        let key = "bundle:".to_string() + id;
+        let content = storage.get(key.as_str()).bytes().await?;
+        return match content {
+            Some(bytes) => match Package::from_bytes(bytes) {
+                Ok(Package { manifest: Some(manifest), .. }) => {
+                    Response::from_json(&manifest)
+                },
+                Ok(_) => {
+                    Response::from_json(&json!(null))
+                },
+                Err(err) => {
+                    Response::error(err.to_string(), 500)
+                },
+            },
+            None => Response::error("Bundle not found", 404),
+        };
+    }
+    Response::error("Bad Request", 400)
+}
+
+async fn upload_bundle_content(mut req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let storage = ctx.kv("KV_BUNDLE_STORAGE")?;
+    if let Some(id) = ctx.param("id") {
+        let form = req.form_data().await?;
+        if let Some(FormEntry::File(file)) = form.get("content") {
+            console_log!("{} {}", file.name(), file.type_());
+            let key = "bundle:".to_string() + id;
+            let bytes = file.bytes().await?;
+            return match Package::from_bytes(bytes.clone()) {
+                Ok(_) => {
+                    storage.put_bytes(key.as_str(), bytes.as_slice())?.execute().await?;
+                    Response::empty()
+                },
+                Err(err) => {
+                    Response::error(err.to_string(), 400)
+                },
+            };
+        }
+    }
+    Response::error("Bad Request", 400)
+}
+
+async fn connect_bundle_hostname(mut req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let storage = ctx.kv("KV_BUNDLE_STORAGE")?;
+    if let Some(id) = ctx.param("id") {
+        let form = req.form_data().await?;
+        return match form.get("hostname") {
+            Some(FormEntry::Field(hostname)) => {
+                let key = "hostname:".to_string() + hostname.as_str();
+                storage.put(key.as_str(), id.as_str())?.execute().await?;
+                Response::empty()
+            },
+            Some(_) => {
+                Response::error("\"hostname\" must be a string", 404)
+            },
+            None => {
+                Response::error("\"hostname\" is required", 404)
+            },
+        };
+    }
+    Response::error("Bad Request", 400)
+}
+
