dc1f42c3de226f3df5cd0d42886777d4a930e633
Hyeseong Kim <tim@daangn.com>
2022-07-22 03:40:28 +0900

    fix to parse manifest correctly on parsing package

--- karrotmini-miniapp-package/src/lib.rs
+++ karrotmini-miniapp-package/src/lib.rs
@@ -1,2 +1,2 @@
-pub mod result;
+mod result;
 pub mod package;
--- karrotmini-miniapp-package/src/package.rs
+++ karrotmini-miniapp-package/src/package.rs
@@ -5,6 +5,9 @@ use zip::ZipArchive;
 
 use crate::result::{PackageResult, PackageError};
 
+pub type Error = PackageError;
+pub type Result<T> = PackageResult<T>;
+
 #[derive(Debug)]
 pub struct Package {
     pub manifest: Option<Manifest>,
@@ -13,13 +16,19 @@ pub struct Package {
 }
 
 impl Package {
-    pub fn from_bytes(bytes: Vec<u8>) -> PackageResult<Self> {
+    pub fn from_bytes(bytes: Vec<u8>) -> Result<Self> {
         let mut archive = ZipArchive::new(Cursor::new(bytes))?;
         let mut buf: Vec<u8> = Vec::new();
+
         let _ = archive.by_name(manifest::MANIFEST_FILENAME).map(|mut file| {
             file.read_to_end(&mut buf).unwrap();
         });
-        let manifest = Manifest::try_from(buf).ok();
+
+        let manifest = match buf.is_empty() {
+            true => None,
+            false => Some(Manifest::try_from(buf)?),
+        };
+
         Ok(Self {
             manifest,
             inner: archive,
@@ -33,16 +42,16 @@ impl Package {
                 let mut buf: Vec<u8> = Vec::new();
                 file.read_to_end(&mut buf).unwrap();
                 Some(buf)
-            }
+            },
             Err(..) => None,
         }
     }
 }
 
 impl<'a> TryFrom<&'a [u8]> for Package {
-    type Error = PackageError;
+    type Error = Error;
 
-    fn try_from(bytes: &'a [u8]) -> Result<Self, Self::Error> {
+    fn try_from(bytes: &'a [u8]) -> Result<Self> {
         Package::from_bytes(Vec::from(bytes))
     }
 }
@@ -75,6 +84,17 @@ mod test {
 
         let manifest = package.unwrap().manifest;
         assert!(manifest.is_none());
+    }
+
+    #[test]
+    fn invalid_manifest() {
+        use super::Package;
+
+        let mut v = Vec::new();
+        v.extend_from_slice(include_bytes!("../tests/data/invalid_manifest.zip"));
+
+        let package = Package::from_bytes(v);
+        assert!(package.is_err());
     }
 
     #[test]
